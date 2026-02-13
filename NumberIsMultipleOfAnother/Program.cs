7adbd27f1ddf9ea188f35398468f183b3174047a
DEIBIS MANUEL DIAZ GARCIA <deibisddiaz123@gmail.com>
2026-02-13 18:09:37 -0500

    create blank solucion

---
+++ NumberIsMultipleOfAnother/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        do
+        {
+            Console.Write("Ingrese primer número (o escriba salir): ");
+            string entrada = Console.ReadLine().ToLower();
+
+            if (entrada == "salir")
+            {
+                Console.WriteLine("Programa finalizado.");
+                break;
+            }
+
+            int num1 = int.Parse(entrada);
+
+            Console.Write("Ingrese segundo número: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            if (num1 % num2 == 0)
+            {
+                Console.WriteLine("El número: " + num2 + ", es múltiplo de: " + num1);
+            }
+            else
+            {
+                Console.WriteLine("El número: " + num2 + ", NO es múltiplo de: " + num1);
+            }
+
+            Console.WriteLine(); // espacio
+
+        } while (true);
+    }
+}
---
+++ OrderingOfThreeNumbers/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        do
+        {
+            Console.Write("Ingrese primer número (o escriba salir): ");
+            string entrada1 = Console.ReadLine().ToLower();
+
+            if (entrada1 == "salir")
+            {
+                Console.WriteLine("Programa finalizado.");
+                break;
+            }
+
+            int num1 = int.Parse(entrada1);
+
+            Console.Write("Ingrese segundo número: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese tercer número: ");
+            int num3 = int.Parse(Console.ReadLine());
+
+            int mayor, medio, menor;
+
+            // Buscar mayor
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                    mayor = num1;
+                else
+                    mayor = num3;
+            }
+            else
+            {
+                if (num2 > num3)
+                    mayor = num2;
+                else
+                    mayor = num3;
+            }
+
+            // Buscar menor
+            if (num1 < num2)
+            {
+                if (num1 < num3)
+                    menor = num1;
+                else
+                    menor = num3;
+            }
+            else
+            {
+                if (num2 < num3)
+                    menor = num2;
+                else
+                    menor = num3;
+            }
+
+            medio = num1 + num2 + num3 - mayor - menor;
+
+            Console.WriteLine("El número mayor es: " + mayor);
+            Console.WriteLine("El número del medio es: " + medio);
+            Console.WriteLine("El número menor es: " + menor);
+
+            Console.WriteLine(); // Espacio visual
+
+        } while (true);
+    }
+}
+
+
+
+
