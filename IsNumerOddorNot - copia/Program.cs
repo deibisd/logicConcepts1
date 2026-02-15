7bf7059126853f530ed3a70931166e78b048279a
DEIBIS MANUEL DIAZ GARCIA <deibisddiaz123@gmail.com>
2026-02-14 19:18:17 -0500

    Create blank solucion

---
+++ IsNumerOddorNot - copia/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+do
+{
+    Console.Write("\nIngrese un número o escriba 'salir' para terminar: ");
+    string input = Console.ReadLine().Trim().ToLower();
+
+    if (input == "salir")
+    {
+        Console.WriteLine("Programa finalizado.");
+        break;
+    }
+
+    bool valido = int.TryParse(input, out int numero);
+    if (!valido)
+    {
+        Console.WriteLine("Entrada inválida. Intente nuevamente.");
+        continue;
+    }
+
+    if (numero % 2 == 0)
+    {
+        Console.WriteLine($"El número {numero} es PAR.");
+    }
+    else
+    {
+        Console.WriteLine($"El número {numero} es IMPAR.");
+    }
+
+} while (true);
