2bfc31363c7a1d782f2ec0bd1a93a93c156266c0
DEIBIS MANUEL DIAZ GARCIA <deibisddiaz123@gmail.com>
2026-02-18 18:55:52 -0500

    create blank solucion

---
+++ LeapYear/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string repetir;
+        do
+        {
+            Console.Write("Ingrese año: ");
+            int anio = int.Parse(Console.ReadLine());
+
+            if ((anio % 4 == 0 && anio % 100!= 0) || (anio % 400 == 0))
+                Console.WriteLine("El año: " + anio + ", si es bisiesto");
+            else
+                Console.WriteLine("El año: " + anio + ", no es bisiesto");
+
+            Console.WriteLine("¿Continuar? (s/n)");
+            repetir = Console.ReadLine()!.ToLower();
+        } while (repetir == "s");
+    }
+}
+
