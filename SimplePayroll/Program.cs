f5858c0cc8261a60ede6488dbea5eb13a2300860
DEIBIS MANUEL DIAZ GARCIA <deibisddiaz123@gmail.com>
2026-02-18 19:18:01 -0500

    create blank solucion

---
+++ SimplePayroll/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string continuar;
+        do
+        {
+            Console.Clear();
+            Console.Write("Ingrese nombre: ");
+            string nombre = Console.ReadLine();
+
+            Console.Write("Ingrese número de horas trabajadas: ");
+            double horas = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese valor hora: ");
+            double valor = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese valor salario mínimo mensual: ");
+            double minimo = double.Parse(Console.ReadLine());
+
+            double total = horas * valor;
+
+            Console.WriteLine("Nombre: " + nombre);
+            if (total > minimo)
+            {
+                Console.WriteLine("Salario mensual: " + total);
+            }
+
+            Console.WriteLine("¿Desea ingresar otro empleado? (si/no)");
+            continuar = Console.ReadLine().ToLower();
+
+        } while (continuar == "si");
+    }
+}
